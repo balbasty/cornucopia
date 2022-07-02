11b1a8a50b9a989aaaaece7c537d73906fab4c08
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2022-07-02 15:24:10 -0400

    DOC: sphinx

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+extensions = [
+    'myst_parser', 
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.coverage', 
+    'sphinx.ext.napoleon',
+    'autoapi.extension',
+]
+
+autoapi_dirs = ['../cornucopia']
+
+html_theme = 'sphinx_book_theme'
