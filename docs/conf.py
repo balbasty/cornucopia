4f533cdcd04d3de0eb7208924401042c0c7ab86b
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2022-10-27 17:32:07 -0400

    FIX(doc): try myst_docstring

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,12 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+# Module search path
+import os
+import sys
+here = Path(__file__).absolute().parent
+sys.path.insert(0, str(here.parent))
+sys.path.insert(0, str(here/'extensions'))
 
 # -- Project information -----------------------------------------------------
 
@@ -29,7 +27,8 @@ author = 'Yael Balbastre'
 # ones.
 extensions = [
     'myst_nb',
-    'sphinx.ext.autodoc',
+    'myst_docstring',
+    'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'autoapi.extension',
