60c5d57c6dbf2286de40677e159b0436171a26ee
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2023-06-01 18:41:32 -0400

    FEAT(intensity): slicewise or slabwise bias fields

--- cornucopia/intensity.py
+++ cornucopia/intensity.py
@@ -1,5 +1,6 @@
 __all__ = ['AddFieldTransform', 'MultFieldTransform', 'BaseFieldTransform',
            'RandomAddFieldTransform', 'RandomMultFieldTransform',
+           'RandomSlicewiseMultFieldTransform',
            'GlobalMultTransform', 'RandomGlobalMultTransform',
            'GlobalAdditiveTransform', 'RandomGlobalAdditiveTransform',
            'GammaTransform', 'RandomGammaTransform',
@@ -7,15 +8,18 @@ __all__ = ['AddFieldTransform', 'MultFieldTransform', 'BaseFieldTransform',
 
 import torch
 from torch.nn.functional import interpolate
+import math
 import interpol
 from .base import Transform, RandomizedTransform, prepare_output
 from .random import Sampler, Uniform, RandInt, Fixed, make_range
-from .utils.py import ensure_list
+from .utils.py import ensure_list, positive_index
 
 
 class BaseFieldTransform(Transform):
+    """Base class for transforms that sample a smooth field"""
 
     def __init__(self, shape=5, vmin=0, vmax=1, order=3,
+                 slice=None, thickness=None,
                  *, shared=False, **kwargs):
         """
 
@@ -29,6 +33,12 @@ class BaseFieldTransform(Transform):
             Maximum value
         order : int
             Spline order
+        slice : int
+            Slice direction, if slicewise.
+        thickness : int
+            Slice thickness, if slicewise.
+            Note that `shape` will be scaled along the slice direction
+            so that the number of nodes is approximately preserved.
         returns : [list or dict of] {'input', 'output', 'field'}
             Which tensor(s) to return
         shared : bool
@@ -39,10 +49,83 @@ class BaseFieldTransform(Transform):
         self.vmax = vmax
         self.vmin = vmin
         self.order = order
+        self.slice = slice
+        self.thickness = thickness
 
     def get_parameters(self, x):
+
+        # slicewise bias
+        if self.slice is not None:
+            ndim = x.ndim - 1
+            fullshape = x.shape[1:]
+            slice = positive_index(self.slice, ndim)
+            thickness = self.thickness or 1
+            nb_slices = int(math.ceil(x.shape[1+slice] / thickness))
+
+            shape_orig, slice_orig = self.shape, self.slice
+            smallshape = ensure_list(self.shape, ndim)
+            smallshape[slice] = int(math.ceil(smallshape[slice] / nb_slices))
+            smallshape = [min(small, full) for small, full
+                          in zip(smallshape, fullshape)]
+            self.shape, self.slice = smallshape, None
+
+            if self.thickness == 1:
+                # bias independent across slices -> batch it
+                self.shape = self.shape[:slice] + self.shape[slice+1:]
+                batch = len(x)
+                x = x.movedim(1+slice, 1)
+                x = x.reshape([-1, *x.shape[2:]])
+                b = self.get_parameters(x)
+                b = b.reshape([batch, -1, *b.shape[1:]])
+                b = b.movedim(1, 1+slice)
+
+            elif x.shape[1+slice] % thickness == 0:
+                # shape divisible by thickness -> unfold and batch
+                batch, *fullshape = x.shape
+                x = x.unfold(1+slice, thickness, thickness)
+                x = x.movedim(1+slice, 1).movedim(-1, 2+slice)
+                x = x.reshape([-1, *x.shape[2:]])
+                b = self.get_parameters(x)
+                b = b.reshape([batch, -1, *b.shape[1:]])
+                b = b.movedim(1, 1+slice)
+                b = b.reshape([batch, *fullshape])
+
+            else:
+                # otherwise, the input is not exactly divisible by thickness
+                x0, b0 = x, torch.empty_like(x)
+                b0 = b0.movedim(1+slice, 1)
+                batch, *fullshape = x.shape
+
+                # use same strategy as before for all but last slice
+                x = x.unfold(1+slice, thickness, thickness)
+                fullshape[slice] = x.shape[1+slice] * x.shape[-1]
+                x = x.movedim(1+slice, 1).movedim(-1, 2+slice)
+                x = x.reshape([-1, *x.shape[2:]])
+                b = self.get_parameters(x)
+                b = b.reshape([batch, -1, *b.shape[1:]])
+                b = b.movedim(1, 1+slice)
+                b = b.reshape([batch, *fullshape])
+                b = b.movedim(1+slice, 1)
+
+                # copy into the larger placeholder
+                b0[:, :b.shape[1]].copy_(b)
+
+                # process last slice
+                x = x0.movedim(1+slice, 1)[:, b.shape[1]:].movedim(1, 1+slice)
+                b1 = self.get_parameters(x).movedim(1+slice, 1)
+                b0[:, b.shape[1]:].copy_(b1)
+
+                # reorder
+                b = b0.movedim(1, 1+slice)
+
+            self.shape, self.slice = shape_orig, slice_orig
+            return b
+
+        # global bias
         batch, *fullshape = x.shape
         smallshape = ensure_list(self.shape, len(fullshape))
+        smallshape = [min(small, full) for small, full
+                      in zip(smallshape, fullshape)]
         backend = dict(dtype=x.dtype, device=x.device)
         if not backend['dtype'].is_floating_point:
             backend['dtype'] = torch.get_default_dtype()
@@ -71,7 +154,7 @@ class MultFieldTransform(BaseFieldTransform):
 class RandomMultFieldTransform(RandomizedTransform):
     """Random multiplicative bias field transform"""
 
-    def __init__(self, shape=8, vmax=1, order=3, shared=False):
+    def __init__(self, shape=8, vmax=1, order=3, *, shared=False):
         """
         Parameters
         ----------
@@ -88,6 +171,78 @@ class RandomMultFieldTransform(RandomizedTransform):
                       shape=RandInt.make(make_range(2, shape)),
                       order=Fixed.make(order))
         super().__init__(MultFieldTransform, kwargs, shared=shared)
+
+
+class RandomSlicewiseMultFieldTransform(RandomizedTransform):
+    """Random multiplicative bias field transform, per slice or slab"""
+
+    def __init__(self, shape=8, vmax=1, order=3, slice=None, thickness=32,
+                 shape_through=None, *, shared=False):
+        """
+        Parameters
+        ----------
+        shape : Sampler or int
+            Sampler or Upper bound for number of control points
+        vmax : Sampler or float
+            Sampler or Upper bound for maximum value
+        order : int
+            Spline order
+        slice : int
+            Slice axis. If None, sample one randomly
+        thickness:
+            Sampler or Upper bound for slice thickness
+        shape_through : Sampler or int
+            Sampler or Upper bound for number of control points
+            along the slice direction. If None, same as `shape`.
+        shared : bool
+            Whether to share random parameters across channels
+        """
+        if shape_through is not None:
+            shape_through = RandInt.make(make_range(1, shape_through))
+        kwargs = dict(vmax=Uniform.make(make_range(0, vmax)),
+                      shape=RandInt.make(make_range(2, shape)),
+                      order=Fixed.make(order),
+                      slice=slice,
+                      thickness=RandInt.make(make_range(0, thickness)),
+                      shape_through=shape_through)
+        super().__init__(MultFieldTransform, kwargs, shared=shared)
+
+    def get_parameters(self, x):
+        ndim = x.ndim - 1
+        shape_orig = self.sample['shape']
+        shape_through_orig = self.sample['shape_through']
+        slice_orig = self.sample['slice']
+        thickness_orig = self.sample['thickness']
+
+        if self.sample['slice'] is None:
+            self.sample['slice'] = RandInt(x.ndim-2)
+        if shape_through_orig is not None:
+            if isinstance(self.sample['slice'], Sampler):
+                self.sample['slice'] = self.sample['slice']()
+            self.sample['slice'] = positive_index(self.sample['slice'], ndim)
+            shape = shape_orig
+            if isinstance(shape, Sampler):
+                shape = shape(ndim)
+            shape = list(ensure_list(shape, ndim))
+            thickness = thickness_orig
+            if isinstance(thickness, Sampler):
+                thickness = thickness()
+            shape_through = shape_through_orig
+            if isinstance(shape_through, Sampler):
+                shape_through = shape_through()
+            shape_through *= int(math.ceil(x.shape[1+self.sample['slice']] / thickness))
+            shape[self.sample['slice']] = shape_through
+            print(shape)
+            self.sample['shape'] = shape
+        self.sample.pop('shape_through')
+
+        out = super().get_parameters(x)
+
+        self.sample['shape'] = shape_orig
+        self.sample['shape_through'] = shape_through_orig
+        self.sample['slice'] = slice_orig
+        self.sample['thickness'] = thickness_orig
+        return out
 
 
 class AddFieldTransform(BaseFieldTransform):
--- cornucopia/utils/py.py
+++ cornucopia/utils/py.py
@@ -309,3 +309,8 @@ def ind2sub(ind: int, shape: List[int]) -> List[int]:
             sub[d] = remainder(sub[d], stride[d-1])
         sub[d] = sub[d] // stride[d]
     return sub
+
+
+def positive_index(index, size):
+    """Transform negative indices into positive indices"""
+    return size + index if index < 0 else index
