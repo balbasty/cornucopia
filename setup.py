381f588b3abb36073de06ad9084c351624f50a14
balbasty <yael.balbastre@gmail.com>
2024-10-24 17:47:11 +0100

    Use config.cfg for everything

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
-import versioneer
+import os
+import sys
+from setuptools import setup
 
-setup(
-    packages=find_packages(),
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-)
+# The following versioneer hack copied from dandi-cli
+# https://github.com/dandi/dandi-cli/blob/master/setup.py
+
+
+# This is needed for versioneer to be importable when building with PEP 517.
+# See <https://github.com/warner/python-versioneer/issues/193> and links
+# therein for more information.
+sys.path.insert(0, os.path.dirname(__file__))
+
+try:
+    import versioneer
+
+    setup_kw = {
+        "version": versioneer.get_version(),
+        "cmdclass": versioneer.get_cmdclass(),
+    }
+except ImportError:
+    # see https://github.com/warner/python-versioneer/issues/192
+    print("WARNING: failed to import versioneer, falling back to no version for now")
+    setup_kw = {}
+
+setup(**setup_kw)
